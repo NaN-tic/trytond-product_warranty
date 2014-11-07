f69692f176aff16fd1aa1698100f0adf0a523782
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:33:41 +0100

    Cleanup tests

--- tests/test_product_warranty.py
+++ tests/test_product_warranty.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class ProductWarrantyTestCase(unittest.TestCase):
-    '''
-    Test Product Warranty module.
-    '''
+    'Test Product Warranty module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('product_warranty')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('product_warranty')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
